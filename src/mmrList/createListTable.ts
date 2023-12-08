e7b6b2306afaea390a40b7763d125304044a59c4
ronron6566 <ronron6566@gmail.com>
2023-12-09 00:48:06 +0900

    Fix name to loungeId

--- src/mmrList/createListTable.ts
+++ src/mmrList/createListTable.ts
@@ -3,9 +3,9 @@ import { getAverageMmr, getMmrList } from "./getMmrList";
 import { PlayerData, mmrListtable } from "./model";
 import { Table } from "embed-table";
 
-export const createEmbedMmrList = async (playerIds:string[]) :Promise<EmbedBuilder>=> {
+export const createEmbedMmrList = async (loungeIds:string[]) :Promise<EmbedBuilder>=> {
 
-    const mmrList = await getMmrList(playerIds);
+    const mmrList = await getMmrList(loungeIds);
     const table = await addMmrListToTable(mmrList);
     const embed = new EmbedBuilder().setFields(table.toField());
     embed.setColor('Random')
--- src/mmrList/index.ts
+++ src/mmrList/index.ts
@@ -1,18 +1,18 @@
 import { Message } from "discord.js";
 import { createEmbedMmrList } from "./createListTable";
-import { playerIds } from "../../env/env";
-import { getPlayerIdsByDiscordIds } from "./model";
+import { loungeIds } from "../../env/env";
+import { getLoungeIdsByDiscordIds } from "./model";
 
 export const mmrListHandler = async (message: Message) => {
-    const embedMmrList = await createEmbedMmrList(playerIds);  
+    const embedMmrList = await createEmbedMmrList(loungeIds);  
     message.channel.send({ embeds: [embedMmrList] });
 }
 
 export const makeTeamHandler = async (message: Message) => {
     const targetDiscordIds = (message.content.match(/<@(.*?)>/g) || []).map(match => match.slice(2, -1));
-    const targetPlayerIds = getPlayerIdsByDiscordIds(targetDiscordIds)
-    if (targetPlayerIds.length > 0) {
-        const embedMmrList = await createEmbedMmrList(targetPlayerIds);  
+    const targetLoungeIds = getLoungeIdsByDiscordIds(targetDiscordIds)
+    if (targetLoungeIds.length > 0) {
+        const embedMmrList = await createEmbedMmrList(targetLoungeIds);  
         message.channel.send({ embeds: [embedMmrList] });
     } else {
         message.channel.send('プレイヤーを指定してください');
